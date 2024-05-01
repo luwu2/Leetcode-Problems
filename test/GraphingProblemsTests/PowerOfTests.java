4f31a2c2e7ba5b20a8e3ba4d7c85d07503a46d20
luwu2 <luccawu000@gmail.com>
2024-04-30 20:10:39 -0400

    update2

--- test/GraphingProblemsTests/PowerOfTests.java
+++ test/GraphingProblemsTests/PowerOfTests.java
@@ -3,14 +3,15 @@ package GraphingProblemsTests;
 import org.junit.Test;
 
 import static RecursionProblems.PowerOf.powerOfN;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class PowerOfTests {
   @Test
   public void testPowerOf() {
     assertTrue(powerOfN(27, 3));
-    assertTrue(!powerOfN(100, 5));
-    assertTrue(!powerOfN(0,1));
+    assertFalse(powerOfN(100, 5));
+    assertFalse(powerOfN(0, 1));
   }
 
 }
